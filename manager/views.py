e8ae614cb047f2798b36686ccd8c3b82f55b3665
K <kitkatapioca@gmail.com>
2025-09-03 17:02:21 -0400

    primeros templates
    Correcion de settings, implementacion de la primera version de los templates del crud de propiedades, implementacion de vistas simples para los templates

---
+++ manager/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def registro(request):
+    return render(request, 'templatesApp/registrarPropiedades.html')
+
+def propiedades(request):
+    return render(request, 'templatesApp/propiedades.html')
