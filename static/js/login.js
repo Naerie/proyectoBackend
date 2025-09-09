9d60eea6c9f9e9391d32c0cd294e21404ae0c6f8
marianarondone <marianarondone@gmail.com>
2025-09-09 20:27:22 -0300

    Adicion de suscripcion

--- static/js/login.js
+++ static/js/login.js
@@ -4,6 +4,7 @@ let intentos = 4; // número máximo de intentos
       let email = document.getElementById('email').value;
       let passwd = document.getElementById('passwd').value;
 
+<<<<<<< Updated upstream
       if (email === 'admin@gmail.com' && passwd === '123456') {
           consolelog('Inicio de sesión exitoso.');
           window.location.href = "http://127.0.0.1:8000/propiedades";
@@ -19,3 +20,23 @@ let intentos = 4; // número máximo de intentos
           }
       }
   }
+=======
+    let email = document.getElementById('email').value;
+    let passwd = document.getElementById('password').value;
+
+    if (email === 'admin@gmail.com' && passwd === '123456') {
+        console.log('Inicio de sesión exitoso.');
+        window.location.href = "http://127.0.0.1:8000/manage/";
+    } else {
+        intentos--; 
+        if (intentos > 0) {
+            alert(`Contraseña o correo inválido. Te quedan ${intentos} intentos.`);
+        } else {
+            alert('Has excedido el número de intentos.');
+            document.getElementById('email').disabled = true;
+            document.getElementById('password').disabled = true;
+            document.getElementById('btnLogin').disabled = true;
+        }
+    }
+}
+>>>>>>> Stashed changes
