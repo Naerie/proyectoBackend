6b4f5df36b6f35968d1bb496125b163acb5e7bf6
K <kitkatapioca@gmail.com>
2025-10-08 19:32:38 -0300

    Correccion de errores, eliminacion de codigo comentado

--- main/models.py
+++ main/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Contacto(models.Model):
     nombre = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
     nTelefono = models.CharField(max_length=12)
     mensaje = models.TextField()
