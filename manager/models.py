5c1d8e666739a3ad4f3359233cc53f92197f29b3
K <kitkatapioca@gmail.com>
2025-10-08 18:02:28 -0300

    model de propiedades
    se cambio la manera en que el formulario del registro de propiedades trabaja ya que se incorporó el model de propiedades

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Propiedad(models.Model):
+    titulo = models.CharField(max_length=50)
+    thumb = models.ImageField()
+    carrusel = models.ImageField()
+    nBanos = models.IntegerField()
+    nHabitaciones = models.IntegerField()
+    superficie = models.FloatField()
+    ubicacion = models.CharField(max_length=50)
+    comuna = models.CharField(max_length=50)
+    tipo = models.CharField(max_length=50)
+    estado = models.CharField(max_length=50) 
+    precio = models.CharField(max_length=50)
+    descripcion = models.TextField()
