596513c2479c4fe8cb2e81aa3e7b323d2164f5bf
K <176355154+Naerie@users.noreply.github.com>
2025-10-13 12:01:26 -0300

    creacion tablas admin formulario contacto

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from main.models import Contacto
 # Register your models here.
+
+class ContactoAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'email', 'nTelefono', 'mensaje']
+
+admin.site.register(Contacto, ContactoAdmin)
