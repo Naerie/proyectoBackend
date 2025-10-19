d3da7d792479141d732c20d75358e7314f8dbd1b
K <176355154+Naerie@users.noreply.github.com>
2025-10-19 15:23:37 -0300

    gestion en admin

--- manager/admin.py
+++ manager/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from manager.models import Propiedad
+from manager.models import Propiedad, TiposPropiedades, OperacionesPropiedades, EstadosPropiedades, Comunas
 
 # Register your models here.
 
@@ -24,4 +24,25 @@ class PropiedadAdmin(admin.ModelAdmin):
         'piscina'
     ]
 
+class TiposPropiedadesAdmin(admin.ModelAdmin):
+    list_display = [
+        'tipoPropiedad'
+    ]
+class OperacionesPropiedadesAdmin(admin.ModelAdmin):
+    list_display = [
+        'operacion'
+    ]
+class EstadosPropiedadesAdmin(admin.ModelAdmin):
+    list_display = [
+        'estado'
+    ]
+
+class ComunasAdmin(admin.ModelAdmin):
+    list_display = [
+        'comuna'
+    ]
 admin.site.register(Propiedad, PropiedadAdmin)
+admin.site.register(TiposPropiedades, TiposPropiedadesAdmin)
+admin.site.register(OperacionesPropiedades, OperacionesPropiedadesAdmin)
+admin.site.register(EstadosPropiedades, EstadosPropiedadesAdmin)
+admin.site.register(Comunas, ComunasAdmin)
